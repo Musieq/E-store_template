a169ce1b47cc64678608ba1694118e3f95114c57
Musieq <m.musielak97@gmail.com>
2021-04-28 16:13:38 +0200

    Images are now inserted into database

--- admin/images.php
+++ admin/images.php
@@ -1,10 +1,71 @@
 <?php
+$errors = [];
+$phpFileUploadErrors = [
+    1 => 'The uploaded file exceeds the upload_max_filesize directive in php.ini',
+    2 => 'The uploaded file exceeds the MAX_FILE_SIZE directive that was specified in the HTML form',
+    3 => 'The uploaded file was only partially uploaded',
+    4 => 'No file was uploaded',
+    6 => 'Missing a temporary folder',
+    7 => 'Failed to write file to disk.',
+    8 => 'A PHP extension stopped the file upload.',
+];
+
+/** Upload image to server and database **/
+if (isset($_POST['addImageButton'])) {
+    $imageUpload = $_FILES['addImage'];
+
+    // Check if there are no errors when uploading file
+    if ($imageUpload['error'] == 0) {
+        $imageName = $imageUpload['name'];
+        $imageTmpName = $imageUpload['tmp_name'];
+        $imageType = $imageUpload['type'];
+        $imageSize = $imageUpload['size'];
+        $imageExtension = pathinfo($imageName, PATHINFO_EXTENSION);
+
+        // Check if uploaded file's type is allowed
+        $allowedTypes = ['image/jpeg', 'image/jpg', 'image/png'];
+        if (in_array($imageType, $allowedTypes)) {
+            // Get image alt and title from form
+            $imageAlt = $_POST['addImageAlt'];
+            $imageTitle = $_POST['addImageTitle'];
+            $imageTitle = empty($imageTitle) ? str_replace('.'.$imageExtension,'', $imageName) : $imageTitle;
+
+            // Generate unique name for file
+            $uniqueImageName = uniqid('',true).'.'.$imageExtension;
+
+            // Create folders for storing images if doesn't exist
+            $imageDestination = 'uploaded_images/'.date('Y').'/'.date('m');
+            if (!file_exists('../'.$imageDestination)) {
+                mkdir('../'.$imageDestination, 0777, true);
+            }
+
+            $imageFullDestination = $imageDestination.'/'.$uniqueImageName;
+
+            // Upload image
+            move_uploaded_file($imageTmpName, '../'.$imageFullDestination);
+
+            // Update database with image informations
+            mysqli_query($db, "INSERT INTO images (unique_name, title, alt, path) VALUES ('$uniqueImageName', '$imageTitle', '$imageAlt', '$imageFullDestination')");
+
+            // TODO copies of image with lower resolutions - function which will also be used in products.php
+
+        } else {
+            array_push($errors, "Wrong file type. Allowed types: jpg, jpeg, png.");
+        }
+    } else {
+        array_push($errors, $phpFileUploadErrors[$imageUpload['error']]);
+    }
+}
 ?>
 
 <div class="row">
 
+    <?php
+    displayErrors($errors);
+    ?>
+
     <div class="col-12">
-        <form class="form-width-700" action="index.php?source=images" method="post">
+        <form class="form-width-700" action="index.php?source=images" method="post" enctype="multipart/form-data">
             <div class="mb-3">
                 <label for="addImage" class="form-label">Select image to upload</label>
                 <input class="form-control" type="file" accept="image/png,image/jpg,image/jpeg" id="addImage" name="addImage">
