7677bcce2aaf90a1d6d647b2abe942ba45c89f30
Musieq <m.musielak97@gmail.com>
2021-05-03 15:38:53 +0200

    Added image search

--- admin/images.php
+++ admin/images.php
@@ -2,6 +2,7 @@
 $errors = [];
 
 
+
 /** Delete image **/
 if (isset($_GET['deleteImageID'])) {
     $deleteImageID = $_GET['deleteImageID'];
@@ -32,6 +33,11 @@ if (isset($_GET['deleteImageID'])) {
             // 5. Delete DB entry
             mysqli_query($db, "DELETE FROM images WHERE id = $deleteImageID");
 
+            // Delete ID of deleted image from URL
+            unset($_GET['deleteImageID']);
+            $cleanURL = http_build_query($_GET);
+            header("Location: index.php?$cleanURL");
+
         } else {
             array_push($errors, "Image with given ID doesn't exist");
         }
@@ -56,21 +62,47 @@ if (isset($_GET['deleteImageID'])) {
 
 
         <!-- Search images by date and title -->
-        <form class="row form-width-700 g-3 mb-3" method="post" action="index.php?source=images">
+        <form class="row form-width-700 g-3 mb-3" method="get" action="index.php">
+            <input type="hidden" name="source" value="images">
+
             <div class="col-md-6">
-                <label for="imageSearch" class="form-label">Search for an image by title</label>
-                <input type="text" class="form-control" id="imageSearch" name="imageSearch">
+                <label for="imageFilterTitle" class="form-label">Search for an image by title</label>
+                <input type="text" class="form-control" id="imageFilterTitle" name="imageFilterTitle" value="<?=$_GET['imageFilterTitle'] ?? ''?>">
             </div>
 
             <div class="col-md-6">
-                <label for="imageDate" class="form-label">Select the date of upload</label>
-                <select class="form-select" id="imageDate" name="imageDate">
-                    <option value="0" selected>Every image</option>
+                <label for="imageFilterDate" class="form-label">Select the date of upload</label>
+                <select class="form-select" id="imageFilterDate" name="imageFilterDate">
+                    <option value="0">Every image</option>
+                    <?php
+                    // Get dates of images uploads
+                    $dateQuery = mysqli_query($db, "SELECT upload_date FROM images");
+                    $dateArray = [];
+                    // Create array with month and year
+                    while ($dateQueryResult = mysqli_fetch_assoc($dateQuery)) {
+                        $date = strtotime($dateQueryResult['upload_date']);
+                        $yearAndMonth = date("F Y", $date);
+                        if (!in_array($yearAndMonth, $dateArray)) {
+                            array_push($dateArray, $yearAndMonth);
+                        }
+                    }
+                    $dateFilterSelected = $_GET['imageFilterDate'] ?? 0;
+                    // Create options to filter by month/year
+                    foreach ($dateArray as $value) {
+                        $date = strtotime($value);
+                        $dateNumeric = date("Ym", $date);
+                        if ($dateFilterSelected == $dateNumeric) {
+                            echo "<option value='$dateNumeric' selected>$value</option>";
+                        } else {
+                            echo "<option value='$dateNumeric'>$value</option>";
+                        }
+                    }
+                    ?>
                 </select>
             </div>
 
             <div class="col-12">
-                <button type="submit" class="btn btn-primary " name="imageFilter">Filter</button>
+                <button type="submit" class="btn btn-primary" name="imageFilterSubmit" value="filter">Filter</button>
             </div>
         </form>
 
@@ -110,7 +142,6 @@ if (isset($_GET['deleteImageID'])) {
             <tbody>
                
                 <?php
-
                 // Variables for pagination
                 $imagesCountQuery = mysqli_query($db, "SELECT COUNT(id) FROM images");
                 $imagesCount = mysqli_fetch_array($imagesCountQuery)[0];
@@ -119,9 +150,38 @@ if (isset($_GET['deleteImageID'])) {
                 $currentPage = $_GET['page'] ?? 1;
                 $offset = ($currentPage - 1) * $limit;
 
-                /** Display all images **/
-                $displayImagesQuery = mysqli_query($db, "SELECT * FROM images ORDER BY upload_date DESC LIMIT $limit OFFSET $offset");
-                while($displayImagesResults = mysqli_fetch_assoc($displayImagesQuery)){
+
+                /** Filter images by title and/or by chosen date **/
+                $titleFilter = '';
+                $dateFilter = 0;
+                if (isset($_GET['imageFilterSubmit'])) {
+                    $titleFilter = $_GET['imageFilterTitle'];
+                    $dateFilter = $_GET['imageFilterDate'];
+                    $yearFilter = substr($dateFilter, 0, 4);
+                    $monthFilter = substr($dateFilter, 4, 2);
+                }
+
+                if ($titleFilter != '' && $dateFilter != 0) {
+                    $titleFilter = '%'.$titleFilter.'%';
+                    $displayImagesQuery = mysqli_prepare($db, "SELECT * FROM images WHERE title LIKE ? AND YEAR(upload_date) LIKE ? AND MONTH(upload_date) LIKE ? ORDER BY upload_date DESC LIMIT $limit OFFSET $offset");
+                    mysqli_stmt_bind_param($displayImagesQuery, "sii",$titleFilter, $yearFilter, $monthFilter);
+                } elseif ($titleFilter != '') {
+                    $titleFilter = '%'.$titleFilter.'%';
+                    $displayImagesQuery = mysqli_prepare($db, "SELECT * FROM images WHERE title LIKE ? ORDER BY upload_date DESC LIMIT $limit OFFSET $offset");
+                    mysqli_stmt_bind_param($displayImagesQuery, "s",$titleFilter);
+                } elseif ($dateFilter != 0) {
+                    $displayImagesQuery = mysqli_prepare($db, "SELECT * FROM images WHERE YEAR(upload_date) LIKE ? AND MONTH(upload_date) LIKE ? ORDER BY upload_date DESC LIMIT $limit OFFSET $offset");
+                    mysqli_stmt_bind_param($displayImagesQuery, "ii",$yearFilter, $monthFilter);
+                } else {
+                    $displayImagesQuery = mysqli_prepare($db, "SELECT * FROM images ORDER BY upload_date DESC LIMIT $limit OFFSET $offset");
+                }
+
+                // Execute prepared statement
+                mysqli_stmt_execute($displayImagesQuery);
+                $displayImagesGetResults = mysqli_stmt_get_result($displayImagesQuery);
+
+                /** Display images **/
+                while($displayImagesResults = mysqli_fetch_assoc($displayImagesGetResults)){
                     $imageID = $displayImagesResults['id'];
                     $imageUniqueName = $displayImagesResults['unique_name'];
                     $imageTitle = $displayImagesResults['title'];
@@ -133,6 +193,9 @@ if (isset($_GET['deleteImageID'])) {
                     $path = pathinfo($fullPath, PATHINFO_DIRNAME);
                     $path .= '/'.pathinfo($fullPath, PATHINFO_FILENAME);
                     $extension = pathinfo($fullPath, PATHINFO_EXTENSION);
+
+                    // Create URL when deleting images after applying filters
+                    $deleteURL = http_build_query(array_merge($_GET,['deleteImageID' => $imageID]));
                     ?>
                     
                     <tr>
@@ -145,7 +208,7 @@ if (isset($_GET['deleteImageID'])) {
                         <td><?=$imageAlt?></td>
                         <td><?=$uploadDate?></td>
                         <td><a href="index.php?source=images&editImageID=<?=$imageID?>">Edit</td>
-                        <td><a href="index.php?source=images&deleteImageID=<?=$imageID?>" class="link-danger delete-image-link" data-bs-toggle="modal" data-bs-target="#modalImageDeleteWarning">Delete</td>
+                        <td><a href="index.php?<?=$deleteURL?>" class="link-danger delete-image-link" data-bs-toggle="modal" data-bs-target="#modalImageDeleteWarning">Delete</td>
                     </tr>
                     
                     <?php  
@@ -166,12 +229,15 @@ if (isset($_GET['deleteImageID'])) {
                     <ul class="pagination justify-content-center">
 
                         <?php
+                        // Unset $_GET['page'] so we can set it when building link. Necessary for pagination when filters are applied.
+                        unset($_GET['page']);
                         if ($pages <= 3) :
                             for ($i = 1; $i <= $pages; $i++) :
+                                $paginationURL = http_build_query(array_merge($_GET,['page' => $i]));
                             ?>
 
                                 <li class="page-item <?php if($i == $currentPage) { echo 'active'; } ?>" <?php if($i == $currentPage) { echo 'aria-current="page"'; } ?>>
-                                    <a class="page-link" href="index.php?source=images&page=<?=$i?>"><?=$i?></a>
+                                    <a class="page-link" href="index.php?<?=$paginationURL?>"><?=$i?></a>
                                 </li>
 
                             <?php
@@ -187,9 +253,10 @@ if (isset($_GET['deleteImageID'])) {
                                 </li>
                             <?php
                             else :
+                                $paginationURL = http_build_query(array_merge($_GET,['page' => 1]));
                             ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="index.php?source=images&page=1">First</a>
+                                    <a class="page-link" href="index.php?<?=$paginationURL?>">First</a>
                                 </li>
                             <?php
                             endif;
@@ -197,9 +264,10 @@ if (isset($_GET['deleteImageID'])) {
 
                             <?php
                             if ($currentPage > 1) :
+                                $paginationURL = http_build_query(array_merge($_GET,['page' => $currentPage-1]));
                             ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="index.php?source=images&page=<?=$currentPage-1?>"><?=$currentPage-1?></a>
+                                    <a class="page-link" href="index.php?<?=$paginationURL?>"><?=$currentPage-1?></a>
                                 </li>
                             <?php
                             endif;
@@ -211,9 +279,10 @@ if (isset($_GET['deleteImageID'])) {
 
                             <?php
                             if ($currentPage < $pages) :
+                                $paginationURL = http_build_query(array_merge($_GET,['page' => $currentPage+1]));
                             ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="index.php?source=images&page=<?=$currentPage+1?>"><?=$currentPage+1?></a>
+                                    <a class="page-link" href="index.php?<?=$paginationURL?>"><?=$currentPage+1?></a>
                                 </li>
                             <?php
                             endif;
@@ -227,9 +296,10 @@ if (isset($_GET['deleteImageID'])) {
                                 </li>
                             <?php
                             else :
+                                $paginationURL = http_build_query(array_merge($_GET,['page' => $pages]));
                                 ?>
                                 <li class="page-item">
-                                    <a class="page-link" href="index.php?source=images&page=<?=$pages?>">Last</a>
+                                    <a class="page-link" href="index.php?<?=$paginationURL?>">Last</a>
                                 </li>
                             <?php
                             endif;
